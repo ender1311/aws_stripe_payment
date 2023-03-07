54b2562be1772493860d58511dda17c09e171c51
Dan <120590237+ender1311@users.noreply.github.com>
2023-03-07 10:34:27 -0500

    initial commit

---
+++ client/script.js
@@ -0,0 +1,21 @@
+function handlePurchaseData(cartItems) {
+  fetch("http://localhost:3000/checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: cartItems,
+    }),
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+      return res.json().then(json => Promise.reject(json))
+    })
+    .then(({ url }) => {
+      window.location = url
+    })
+    .catch(e => {
+      console.error(e.error)
+    })
+}
