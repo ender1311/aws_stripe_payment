fb1c62268b1bf916ad79973eb19d273e28a3ecfb
Dan <120590237+ender1311@users.noreply.github.com>
2023-03-08 00:30:57 -0500

    update

--- server.js
+++ server.js
@@ -95,4 +95,5 @@ app.post("/checkout-session", async (req, res) => {
   }
 })
 
-app.listen(3000)
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
